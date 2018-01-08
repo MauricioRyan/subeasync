dab08c91e6b389f01972364e605609a7f236ff7e
Mauricio Ryan <mauricioryan@gmail.com>
2018-01-08 06:50:38 -0600

    codigo para subir documentos en paralelo

---
+++ subeasync.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"com/client"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+	//"com/logger"
+	"com/models"
+	"strconv"
+	"sync"
+)
+
+func showError(e error) {
+	if e != nil {
+		log.Println(e)
+	}
+}
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+var obs *client.Client = nil
+
+const (
+	host     = "192.168.0.149"
+	port     = 8635
+	user     = "mryan"
+	password = "termita000"
+	dbname   = "noti"
+)
+
+func putObject(contenido []byte, id int64, wg *sync.WaitGroup) {
+	defer wg.Done()
+	input := new(models.PutObjectInput)
+	input.Bucket = "notificaciones"
+	input.Object = "noti1" + strconv.FormatInt(id, 10) + ".pdf"
+	input.ACL = models.PUBLIC_READ_WRITE
+	input.Body = contenido
+	//input.SourceFile = f.Name //"C:\\install.log"
+	//log.Print("pre PutObject")
+	//requst, output := obs.PutObject(input)
+	requst, output := obs.PutObject(input)
+	//log.Print("post PutObject")
+	log.Printf(">%d-%s<", id,input.Object)
+	if requst.Err != nil {
+		log.Printf("err:%s,statusCode:%d,code:%s,message:%s\n", requst.Err, requst.StatusCode, requst.Code, requst.Message)
+		log.Printf("ETag:%s,VersionId:%s\n", output.ETag, output.VersionId)
+	}
+}
+func main() {
+	f, err := os.OpenFile("subenoti.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+	log.Printf("arranco*")
+
+	obs = client.FactoryEx("YGKTCULBWQMWXTB6SHUJ", "sgA4tMY8o41uAXeS4Zzpl6N5SIxQkPNrWWOeFfI9", "", "", "https://obs.sa-argentina-1.telefonicaopencloud.com", true)
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=require",
+		host, port, user, password, dbname)
+	/*--verify-ca sslrootcert=/home/linux/ca-bundle.pem",*/
+
+	db, err := sql.Open("postgres", psqlInfo)
+	check(err)
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	rows, err := db.Query("select id,texto_pdf from notificaciones where texto_pdf is not null order by id desc;")
+	showError(err)
+	check(err)
+	defer rows.Close()
+	for rows.Next() {
+		var wg sync.WaitGroup
+		wg.Add(100)
+		for i:=0; i<100;i++{
+			if i>0 {rows.Next()}
+			var id int64
+			texto := make([]byte, 16000)
+			err = rows.Scan(&id, &texto)
+
+			showError(err)
+			//sube la nofificacion firmada
+			go putObject(texto, id,&wg)
+		}
+		wg.Wait()
+		log.Print("fin grupo")
+	}
+	// get any error encountered during iteration
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
+}
